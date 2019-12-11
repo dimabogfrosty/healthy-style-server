6e928f974b9c6228c90920acc4abd06beb603ce7
dimabogfrosty <dimabogfrosty@gmail.com>
2019-12-11 12:59:28 +0300

    added test for Application.java and added properties for application.properties

---
+++ src/test/java/com/healthy/style/ApplicationTest.java
@@ -0,0 +1,22 @@
+package com.healthy.style;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void applicationContextLoaded() {
+
+    }
+
+    @Test
+    public void applicationContextTest() {
+        Application.main(new String[]{});
+    }
+
+}
