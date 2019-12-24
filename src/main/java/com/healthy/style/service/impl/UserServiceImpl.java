aa19bdb00885692c0a5766c835434ac31545f266
Dima Bogoslovskiy <dimabogfrosty@gmail.com>
2019-12-25 00:38:27 +0300

    removed unnecessary

--- src/main/java/com/healthy/style/service/impl/UserServiceImpl.java
+++ src/main/java/com/healthy/style/service/impl/UserServiceImpl.java
@@ -4,8 +4,6 @@ import com.healthy.style.entity.Achievement;
 import com.healthy.style.entity.Role;
 import com.healthy.style.entity.User;
 import com.healthy.style.entity.User.Gender;
-import com.healthy.style.repository.AchievementRepository;
-import com.healthy.style.repository.RoleRepository;
 import com.healthy.style.repository.UserRepository;
 import com.healthy.style.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,32 +11,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private AchievementRepository achievementRepository;
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    @Autowired
-    public void setRoleRepository(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
-
-    @Autowired
-    public void setAchievementRepository(AchievementRepository achievementRepository) {
-        this.achievementRepository = achievementRepository;
     }
 
     @Override
@@ -78,19 +60,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User entity) {
-       if(entity.getId() != null) {
-           User user = userRepository.getOne(entity.getId());
-           user.getRoles().forEach(role -> role.getUsers().remove(user));
-           user.getAchievements().forEach(achievement -> achievement.getUsers().remove(user));
-       }
-
-        List<Role> roles = roleRepository.findAllById(entity.getRoles().stream().map(Role::getId).collect(toList()));
-        List<Achievement> achievements = achievementRepository
-                .findAllById(entity.getAchievements().stream().map(Achievement::getId).collect(toList()));
-
-        roles.forEach(role -> role.getUsers().add(entity));
-        achievements.forEach(achievement -> achievement.getUsers().add(entity));
-
         return userRepository.saveAndFlush(entity);
     }
 
